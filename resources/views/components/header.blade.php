999ed6fef41b031f99a7851913c097acc62d51f2
FNYukii <forestgreen14689@gmail.com>
2024-04-09 23:30:58 +0900

    feat: Headerをコンポーネント化

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,6 @@
+<header class="bg-white">
+	<div class="mx-auto   w-full px-4 xl:w-[1200px] xl:px-0   py-2">
+
+		<a href="/" class="text-4xl font-light">Thread <span class="text-purple-800">La</span>nd</a>
+	</div>
+</header>
--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -10,13 +10,7 @@
 
 <body class="bg-purple-200">
 
-	<header class="bg-white">
-		<div class="mx-auto   w-full px-4 xl:w-[1200px] xl:px-0   py-2">
-
-			<a href="/" class="text-4xl font-light">Thread <span class="text-purple-800">La</span>nd</a>
-		</div>
-	</header>
-
+	<x-header/>
 
 	<main class="mx-auto   w-full px-4 xl:w-[1200px] xl:px-0">
 
