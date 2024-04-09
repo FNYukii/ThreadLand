95900f6f8a344128491b0899a7f158ffb4c45354
FNYukii <forestgreen14689@gmail.com>
2024-04-09 23:23:16 +0900

    feat: Headerとスレッドカードを作成

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -5,9 +5,36 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Thread Land</title>
+	@vite('resources/css/app.css')
 </head>
 
-<body>
-	<h1>Thread Land</h1>
+<body class="bg-purple-200">
+
+	<header class="bg-white">
+		<div class="mx-auto   w-full px-4 xl:w-[1200px] xl:px-0   py-2">
+
+			<a href="/" class="text-4xl font-light">Thread <span class="text-purple-800">La</span>nd</a>
+		</div>
+	</header>
+
+
+	<main class="mx-auto   w-full px-4 xl:w-[1200px] xl:px-0">
+
+		<h1 class="mt-8 text-2xl font-bold">スレッド</h1>
+
+		<div class="mt-4   grid grid-cols-3 gap-4">
+
+			<div class="bg-white p-6 rounded-xl   flex flex-col gap-2">
+
+				<p class="font-bold">今年買って良かったもの</p>
+				<p>僕はパソコンです。</p>
+				<p class="text-neutral-500">13コメント</p>
+			</div>
+		</div>
+
+
+	</main>
+
 </body>
+
 </html>
