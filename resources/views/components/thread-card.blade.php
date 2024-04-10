52ef21511f070ce8d586aa4cadfa586c9bfbee15
FNYukii <forestgreen14689@gmail.com>
2024-04-10 22:38:35 +0900

    feat: Threadページへのリンクを配置

--- resources/views/components/thread-card.blade.php
+++ resources/views/components/thread-card.blade.php
@@ -1,6 +1,6 @@
-<div class="bg-white p-6 rounded-xl   flex flex-col gap-2">
+<a href="/threads/xxxxx" class="block rounded-xl bg-white p-6    flex flex-col gap-2   hover:bg-neutral-100 hover:scale-95 transition">
 
 	<p class="font-bold">今年買って良かったもの</p>
 	<p>おはようございます。</p>
 	<p class="text-neutral-500">13コメント</p>
-</div>
+</a>
