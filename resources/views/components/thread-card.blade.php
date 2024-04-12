9b258397fdc5f6d33ec788e212131dc124c9185a
FNYukii <forestgreen14689@gmail.com>
2024-04-12 23:32:54 +0900

    clean: 不要なViewコンポーネントを削除

--- resources/views/components/thread-card.blade.php
+++
@@ -1,6 +0,0 @@
-<a href="/threads/xxxxx" class="block rounded-xl bg-white p-6    flex flex-col gap-2   hover:bg-neutral-100 hover:scale-95 transition">
-
-	<p class="font-bold">{{ $thread->title }}</p>
-	<p>xxxxxxx</p>
-	<p class="text-neutral-500">xxコメント</p>
-</a>
--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -25,16 +25,20 @@
 
 		<div class="mt-4   grid grid-cols-3 gap-6">
 
-
 			@foreach($threads as $thread)
-			<x-thread-card :thread="$thread" />
+			<a href="/threads/xxxxx" class="block rounded-xl bg-white p-6    flex flex-col gap-2   hover:bg-neutral-100 hover:scale-95 transition">
+
+				<p class="font-bold">{{ $thread->title }}</p>
+				<p>xxxxxxx</p>
+				<p class="text-neutral-500">xxコメント</p>
+			</a>
 			@endforeach
 
 		</div>
 
 	</main>
 
-	
+
 
 	<x-footer />
 
