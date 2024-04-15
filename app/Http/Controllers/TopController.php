2c27cf3287a5370a4cebec07a11649765ae16f72
FNYukii <forestgreen14689@gmail.com>
2024-04-15 20:42:32 +0900

    feat: 新しい順にthreadsを読み取るようにした

--- app/Http/Controllers/TopController.php
+++ app/Http/Controllers/TopController.php
@@ -10,7 +10,8 @@ class TopController extends Controller
 	public function __invoke(Request $request)
 	{
 		// Threadモデルを通してデータを読み取り
-		$threads = Thread::all();
+		// $threads = Thread::all();
+		$threads = Thread::orderBy('created_at', 'DESC')->get();
 
 		// 変数threadsを渡したViewを表示
 		return view('top')
