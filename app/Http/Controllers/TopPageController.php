f4c79a451748ae789f5000c5b075cb965f5116fd
FNYukii <forestgreen14689@gmail.com>
2024-04-18 23:02:44 +0900

    feat: ThreadCardにコメント数と最初のコメントを表示できた

--- app/Http/Controllers/TopPageController.php
+++ app/Http/Controllers/TopPageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Comment;
 use Illuminate\Http\Request;
 use App\Models\Thread;
 
@@ -12,6 +13,29 @@ class TopPageController extends Controller
 		// Threadモデルを通してデータを読み取り
 		// $threads = Thread::all();
 		$threads = Thread::orderBy('created_at', 'DESC')->get();
+
+
+
+		// 最初のコメントの内容やコメント数の情報も含んだthreadの配列
+		$expandedThreads = array();
+
+		foreach($threads as $thread) {
+
+			// 最初のコメントのtext
+			$firstComment = Comment::where('thread_id', $thread->id)->orderBy('created_at', 'ASC')->first();
+			$firstCommentText = $firstComment->text;
+
+			// コメント数
+			$commentCount = Comment::where('thread_id', $thread->id)->count();
+
+			// 変数expandedThreadを宣言し、thread, firstCommentText, commentCountを格納していく
+			$expandedThread = $thread;
+			$expandedThread->firstCommentText = $firstCommentText;
+			$expandedThread->commentCount = $commentCount;
+
+			// 変数expandedThreadを配列expandedThreadsに追加
+			array_push($expandedThreads, $expandedThread);
+		}
 
 		// 変数threadsを渡したViewを表示
 		return view('top-page')
--- resources/views/top-page.blade.php
+++ resources/views/top-page.blade.php
@@ -33,8 +33,8 @@
 			<a href="/threads/{{ $thread->id }}" class="block rounded-xl bg-white p-6    flex flex-col gap-2   hover:bg-neutral-100 hover:scale-95 transition">
 
 				<p class="font-bold">{{ $thread->title }}</p>
-				<p>xxxxxxx</p>
-				<p class="text-neutral-500">xxコメント</p>
+				<p>{{ $thread->firstCommentText }}</p>
+				<p class="text-neutral-500">{{ $thread->commentCount }} コメント</p>
 			</a>
 			@endforeach
 		</div>
